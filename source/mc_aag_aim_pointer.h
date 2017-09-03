aa4c5920c6ee606487613843cdb87a3c73dc9a41
gdinit <gdinit@gmx.com>
2017-09-03 23:38:46 +0100

    u scripted commit - adding all changes

--- source/mc_aag_aim_pointer.h
+++ source/mc_aag_aim_pointer.h
@@ -32,7 +32,8 @@
 // ================= END: Production Assert Handling =================
 
 #include <SFML/Graphics.hpp>
-#include "3rd-party\json\v.2.1.1\json.hpp"
+// #include "3rd-party\json\v.2.1.1\json.hpp"
+#include "3rd-party/json/v.2.1.1/json.hpp"
 
 //// For GLOBALS & SETTINGS (for now, till they are moved to play_state)
 // #include <memory>
