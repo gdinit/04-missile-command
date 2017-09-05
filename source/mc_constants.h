fba11e3fa2160f9a1055a3efc1ea391b9090139d
gdinit <gdinit@gmx.com>
2017-09-05 10:53:46 +0100

    a mc_constants.h

---
+++ source/mc_constants.h
@@ -0,0 +1,13 @@
+/* mc_constants.h */
+// ===================================80 chars==================================
+
+#ifndef MC_CONSTANTS_H
+#define MC_CONSTANTS_H
+
+constexpr float	MC_CONS_CITY_H { 26.f };
+
+#endif	// MC_CONSTANTS_H
+
+// ===================================80 chars==================================
+/* EOF */
+
