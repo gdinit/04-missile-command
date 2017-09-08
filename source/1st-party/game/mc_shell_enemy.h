6eb2f4e993968f8062b462bfae0fb775de2148ca
gdinit <gdinit@gmx.com>
2017-09-09 00:07:26 +0100

    add blank mc_shell_enemy.h

---
+++ source/1st-party/game/mc_shell_enemy.h
@@ -0,0 +1,14 @@
+/* mc_shell_enemy.h */
+// ===================================80 chars==================================
+
+#ifndef MC_SHELL_ENEMY_H
+#define MC_SHELL_ENEMY_H
+
+#include 
+
+// MC_SHELL_ENEMY_H
+#endif
+
+// ===================================80 chars==================================
+/* EOF */
+
